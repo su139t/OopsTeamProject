a4cca0aefb69fb6549a0d289c31fb51ca12c5029
Arpita Rawat <arpitarawat381@gmail.com>
2026-05-07 14:55:08 +0530

    order. implementation

--- Order.java
+++ Order.java
@@ -1,3 +1,37 @@
+import java.util.ArrayList;
+
 public class Order {
-  
+    int orderCount = 1;
+    int orderId;
+    ArrayList<Products> oderedProducts;
+    int totalAmount;
+    String status;
+
+    Order(ArrayList<Products> cart) {
+        this.orderId = orderCount++;
+        this.oderedProducts = cart;
+        this.totalAmount = calculateAmount();
+        this.status = "Completed";
+
+    }
+
+    int calculateAmount() {
+        int total = 0;
+        for (Products p : oderedProducts) {
+            total += p.price;
+        }
+        return total;
+    }
+
+    void showOrder() {
+        System.out.println("Orders");
+        System.out.println("OrderId : " + orderId);
+        System.out.println("status : " + status);
+
+        for (Products p : oderedProducts) {
+            System.out.println("----" + p.title + "," + p.title);
+        }
+
+        System.out.println("Total Amount : " + totalAmount);
+    }
 }
